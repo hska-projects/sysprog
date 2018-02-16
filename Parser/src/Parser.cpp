c212acd87d760ebbd8e2efb82a11a5ee6fd5be45
eido1011 <eido1011@hs-karlsruhe.de>
2018-02-16 12:04:36 +0100

    testfälle, ausgabe von unexpectedTokenType um token-eintrag erweitert

--- Parser/src/Parser.cpp
+++ Parser/src/Parser.cpp
@@ -88,7 +88,8 @@ void Parser::unexpectedTType(int type) {
 				<< ", current is NULL" << endl;
 	else {
 		cerr << "ERROR: Expected: " << TTdef::getTokenType(type) << ", Got: "
-				<< TTdef::getTokenType(current->getTTnummer()) << endl;
+				<< TTdef::getTokenType(current->getTTnummer())
+				<< " Token is: " << current->getInfoKey()->getString()->getStr() << endl;
 	}
 	cerr << "Stopping..." << endl;
 	exit(1);
